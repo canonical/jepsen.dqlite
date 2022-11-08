08b1826871f06606785d752d3e9acbdd3626d854
Mathieu Borderé <mathieu.bordere@canonical.com>
2022-11-08 11:25:53 +0100

    f

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.2.6-SNAPSHOT"]
+                 [jepsen "0.2.7-SNAPSHOT"]
                  [clj-http "3.10.1"]]
   :main jepsen.dqlite
   :jvm-opts ["-Djava.awt.headless=true"
