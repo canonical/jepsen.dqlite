8ec586befd961b239be85029a97ea6c6c334b81f
NurtureNature <jeff.m.suttor@gmail.com>
2023-03-20 14:34:05 -0700

    align disk nemesis with jepsen.nemesis.combined

--- src/jepsen/dqlite.clj
+++ src/jepsen/dqlite.clj
@@ -80,7 +80,8 @@
                        :pause     {:targets [nil :one :primaries :majority :all]}
                        :kill      {:targets [nil :one :primaries :majority :all]}
                        :interval  (:nemesis-interval opts)
-                       :disk      {:dir     db/data-dir
+                       :disk      {:targets [nil :one :majority :all] ; :primaries not supported
+                                   :dir     db/data-dir
                                    :size-mb 100}}
         local         (:dummy? (:ssh opts))
         os            (if local container/os ubuntu/os)
