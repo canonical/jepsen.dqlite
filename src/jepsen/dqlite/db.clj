cowsql/jepsen.cowsql

db: Make kill! forcefull and stop! graceful
The semantics had gotten accidentally reversed.

Signed-off-by: Free Ekanayaka <free@ekanayaka.io>

src/jepsen/cowsql/db.clj
@@ -96,22 +96,26 @@
                       :-cluster (str/join "," (:nodes test)))))
 
 (defn kill!
-  "Gracefully kill, `SIGTERM`, the Go cowsql test application."
+  "Forcefully kill the Go cowsql test application with `SIGKILL`."
   [test node]
-  (let [signal :SIGTERM]
-    (info "Killing" (app-name node) "with" signal "on" node)
-    (cu/grepkill! signal (app-name node))
+  (let [cmd (app-name node)
+        pidfile (app-pidfile test node)]
+    (cu/stop-daemon! cmd pidfile)
     :killed))
 
 (defn stop!
-  "Stop the Go cowsql test application with `stop-daemon!`,
-   which will `SIGKILL`."
+  "Gracefully stop the Go cowsql test application by sending `SIGTERM`."
   [test node]
-  (if (not (cu/daemon-running? (app-pidfile test node)))
-    :not-running
-    (do
-      (cu/stop-daemon! (app-pidfile test node))
-      :stopped)))
+  (let [cmd (app-name node)
+        pidfile (app-pidfile test node)]
+    (info "Send SIGTERM to" cmd)
+    (timeout 30000 (throw+ {:type    ::kill-timed-out
+                            :cmd     cmd
+                            :pidfile pidfile})
+             (meh (c/exec :killall :-TERM :-w cmd)))
+    (when pidfile
+      (meh (c/exec :rm :-rf pidfile)))
+    :stopped))
 
 (defn members
   "Fetch the cluster members from a random node (who will ask the leader)."

canonical/jepsen.dqlite

src/jepsen/dqlite/db.clj
@@ -74,24 +74,26 @@
                         :-cluster (str/join "," (:nodes test))))))
 
 (defn kill!
-  "Gracefully kill, `SIGTERM`, the Go dqlite test application."
-  [_test node]
-  (let [signal :SIGTERM]
-    (info "Killing" bin "with" signal "on" node)
-    (c/su
-     (cu/grepkill! signal bin))
-    :killed))
-
-(defn stop!
-  "Stop the Go dqlite test application with `stop-daemon!`,
-   which will `SIGKILL`."
+  "Forcefully kill the Go dqlite test application with `SIGKILL`."
   [_test _node]
   (if (not (cu/daemon-running? pidfile))
     :not-running
     (do
       (c/su
        (cu/stop-daemon! pidfile))
-      :stopped)))
+      :killed)))
+
+(defn stop!
+  "Gracefully stop the Go dqlite test application by sending `SIGTERM`."
+  [_test node]
+    (info "Send SIGTERM to" bin)
+    (timeout 30000 (throw+ {:type    ::kill-timed-out
+                            :cmd     bin
+                            :pidfile pidfile})
+             (meh (c/exec :killall :-TERM :-w bin)))
+    (when pidfile
+      (meh (c/exec :rm :-rf pidfile)))
+    :stopped)
 
 (defn members
   "Fetch the cluster members from a random node (who will ask the leader)."
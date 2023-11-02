d77029a78ed0f8b63e2e8e1b35582b34c5d482e3
Free Ekanayaka <free@ekanayaka.io>
2023-11-02 11:35:03 +0000

    db: Make kill! forcefull and stop! graceful
    The semantics had gotten accidentally reversed.
    
    Signed-off-by: Free Ekanayaka <free@ekanayaka.io>

--- src/jepsen/dqlite/db.clj
+++ src/jepsen/dqlite/db.clj
@@ -74,24 +74,26 @@
                         :-cluster (str/join "," (:nodes test))))))
 
 (defn kill!
-  "Gracefully kill, `SIGTERM`, the Go dqlite test application."
-  [_test node]
-  (let [signal :SIGTERM]
-    (info "Killing" bin "with" signal "on" node)
-    (c/su
-     (cu/grepkill! signal bin))
-    :killed))
-
-(defn stop!
-  "Stop the Go dqlite test application with `stop-daemon!`,
-   which will `SIGKILL`."
+  "Forcefully kill the Go dqlite test application with `SIGKILL`."
   [_test _node]
   (if (not (cu/daemon-running? pidfile))
     :not-running
     (do
       (c/su
        (cu/stop-daemon! pidfile))
-      :stopped)))
+      :killed)))
+
+(defn stop!
+  "Gracefully stop the Go dqlite test application by sending `SIGTERM`."
+  [_test node]
+    (info "Send SIGTERM to" bin)
+    (timeout 30000 (throw+ {:type    ::kill-timed-out
+                            :cmd     bin
+                            :pidfile pidfile})
+             (meh (c/exec :killall :-TERM :-w bin)))
+    (when pidfile
+      (meh (c/exec :rm :-rf pidfile)))
+    :stopped)
 
 (defn members
   "Fetch the cluster members from a random node (who will ask the leader)."
